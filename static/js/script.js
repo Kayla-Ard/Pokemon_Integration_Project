a55b22fc4f87763ba689bab31160f775def8c61e
Kayla-Ard <kaylard@yahoo.com>
2024-06-14 18:28:55 -0600

    home page

---
+++ static/js/script.js
@@ -0,0 +1,23 @@
+async function handleSubmit(event) {
+    event.preventDefault();
+
+    const search = event.target.search.value; 
+    console.log(search);
+
+}
+async function fetchPokemonData
+
+function 
+
+
+class PokemonTeam{
+    constructor(){
+        this.team = []
+    }
+
+    getTeamMember = (pokemon) => {
+
+    }
+}
+
+//when we instantiate this object, and then we call this method getTeam.. we will give it an arguement 
